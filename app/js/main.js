fbb3d083bab40f7dacffff924795c14b16439255
NerOneXP <ilaa16119@gmail.com>
2025-01-27 12:45:07 +0300

    final

--- app/js/main.js
+++ app/js/main.js
@@ -13,4 +13,17 @@ $(function(){
     $('.menu__btn').on('click', function(){
         $('.menu__list').toggleClass('menu__list--active');
     });
+
+    $(".menu a").on('click', function (e) {
+
+		e.preventDefault();
+
+
+		var id  = $(this).attr('href'),
+
+		
+			top = $(id).offset().top;
+		
+		$('body,html').animate({scrollTop: top}, 300);
+	});
 });
